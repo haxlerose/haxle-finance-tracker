3c4b0701131e8a37385f6230ef7a43d7f7702e75
haxlerose <chestertonpa@gmail.com>
2018-02-28 17:52:46 -0500

    Fixed case sensative search and  addressed current user in friends list

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,14 @@ class User < ApplicationRecord
   end
 
   def self.matches(field_name, param)
-    where("#{field_name} like ?", "%#{param}%")
+    where("lower(#{field_name}) LIKE ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject{ |user| user.id == self.id }
+  end
+
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
   end
 end
